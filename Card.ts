3838cd33f74e3d9b31e811be718f6de95235729d
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-02 12:35:33 +0930

    Lint and shift async preparation into static method

--- Card.ts
+++ Card.ts
@@ -1,6 +1,6 @@
 export class Card {
-    code: number;
-    dbs: Array<string>;
+    public code: number;
+    public dbs: string[];
     constructor(data, file) {
         this.code = data[0];
         this.dbs = file;
--- Language.ts
+++ Language.ts
@@ -1,32 +1,32 @@
-import { Card } from "./Card";
 import * as octokit from "@octokit/rest";
-import * as sqlite from "sqlite";
-import * as request from "request";
 import * as fs from "fs";
+import * as request from "request";
+import * as sqlite from "sqlite";
+import { Card } from "./Card";
 const GitHub = new octokit();
 
-const reflect = p => p.then(v => ({v, status: true}), e => ({e, status: false}));
+const reflect = (p) => p.then((v) => ({ v, status: true }), (e) => ({ e, status: false }));
 
-function loadDB(file: string): Promise<any> {
-    return new Promise ((resolve, reject) => {
-        sqlite.open(file).then(db => {
-            db.all("select * from datas,texts where datas.id=texts.id").then(data => {
+function loadDB(file: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+        sqlite.open(file).then((db) => {
+            db.all("select * from datas,texts where datas.id=texts.id").then((data) => {
                 resolve(data);
-            }, err => reject(err));
-        }, err => reject(err));
+            }, (err) => reject(err));
+        }, (err) => reject(err));
     });
 }
 
-function downloadDB(file, name) {
+function downloadDB(file, name): Promise<null> {
     return new Promise((resolve, reject) => {
-        let path = "dbs/" + name + "/" + file.name;
-        request(file.download_url, (err, res, body) => {
+        const path = "dbs/" + name + "/" + file.name;
+        request(file.download_url, (err, _, body) => {
             if (err) {
                 reject(err);
             } else {
-                fs.writeFile(path, body, (err) => {
-                    if (err) {
-                        reject(err);
+                fs.writeFile(path, body, er => {
+                    if (er) {
+                        reject(er);
                     } else {
                         resolve();
                     }
@@ -36,91 +36,129 @@ function downloadDB(file, name) {
     });
 }
 
-function downloadRepo(repo: string, name: string): Promise<Array<string>> {
+function downloadRepo(repo: string, name: string): Promise<string[]> {
     return new Promise((resolve, reject) => {
-        let arr: Array<string> = repo.split("/");
-        let arg: any = {
+        const arr: string[] = repo.split("/");
+        const arg: any = {
             owner: arr[0],
-            repo: arr[1]
-        }
+            repo: arr[1],
+        };
         if (arr.length > 2) {
-            arg["path"] = arr.slice(2).join("/");
+            arg.path = arr.slice(2).join("/");
         }
-        GitHub.repos.getContent(arg).then(res => {        
-            let filenames = [];
-			let promises = [];
-			for (let key in res.data)  {
-                let file = res.data[key];
-				if (file.name.endsWith(".cdb")) {
-					promises.push(downloadDB(file, name).then(() => filenames.push(file.name)));
-				}
-			}
-			Promise.all(promises.map(reflect)).then(results => {
-                results.forEach(result => {
+        GitHub.repos.getContent(arg).then((res) => {
+            const filenames = [];
+            const promises = [];
+            for (const key in res.data) {
+                if (res.data.hasOwnProperty(key)) {
+                    const file = res.data[key];
+                    if (file.name.endsWith(".cdb")) {
+                        promises.push(downloadDB(file, name).then(() => filenames.push(file.name)));
+                    }
+                }
+
+            }
+            Promise.all(promises.map(reflect)).then((results) => {
+                results.forEach((result) => {
                     if (!result.status) {
                         console.error("Error downloading database from repo " + repo + "!");
                         console.error(result);
                     }
                 });
                 resolve(filenames);
-            }).catch(e => reject(e));
-        }).catch(e => reject(e));
+            }).catch((e) => reject(e));
+        }).catch((e) => reject(e));
     });
 }
 
+interface ILanguageDataPayload {
+    cards: { [n: number]: Card };
+    setcodes: { [s: string]: string };
+    counters: { [s: string]: string };
+}
+
 export class Language {
-    cards: { [ n : number ] : Card };
-    setcodes: { [ s: string ] : string };
-    constructor(name, config) {
-        let path = "dbs/" + name + "/";
-        if ("setcodeSource" in config) {
-            
-        }
-        if ("localDBs" in config) {
-            config.localDBs.forEach(file => loadDB(path + file).then(data => {
-                for (let cardData of data) {
-                    let card = new Card(cardData, [ file ]);
-                    if (card.code in this.cards) {
-                        let dbs = card.dbs;
-                        dbs.push(file);
-                        card.dbs = dbs;
-                    }
-                    this.cards[card.code] = card;
-                }
-            }, err => {
-                console.error("Could not load database " + file + "!");
-                console.error(err);
-            }));
-        }
-        if ("remoteDBs" in config) {
-            config.remoteDBs.forEach(repo => {
-                downloadRepo(repo, name).then(files => {
-                    files.forEach(file => loadDB(path + file).then(data => {
-                        for (let cardData of data) {
-                            let card = new Card(cardData, [ file ]);
-                            if (card.code in this.cards) {
-                                let dbs = card.dbs;
-                                dbs.push(file);
-                                card.dbs = dbs;
-                            }
-                            this.cards[card.code] = card;
-                        }
-                    }, err => {
-                        console.error("Could not load database " + file + "!");
+    public static prepareData(name, config): Promise<ILanguageDataPayload> {
+        return new Promise((resolve, reject) => {
+            const data: ILanguageDataPayload = {
+                cards: {},
+                counters: {},
+                setcodes: {},
+            };
+            const path = "dbs/" + name + "/";
+            if ("stringsConf" in config) {
+                request(config.stringsConf, (err, _, body) => {
+                    if (err) {
+                        console.error("Error downloading setcodes and counters!");
                         console.error(err);
-                    }));
-                    //delete unused databases
-                    fs.readdir("dbs/" + name, (err, res) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            res.filter(f => !files.includes(f)).forEach(f => {
-                                fs.unlinkSync("dbs/" + name + "/" + f);
-                            });
+                    } else {
+                        for (const line of body.split(/\R/)) {
+                            if (line.startsWith("!setname")) {
+                                const code = line.split(" ")[1];
+                                const nam = line.slice(line.indexOf(code) + code.length + 1);
+                                data.setcodes[code] = nam;
+                            }
+                            if (line.startsWith("!counter")) {
+                                const code = line.split(" ")[1];
+                                const nam = line.slice(line.indexOf(code) + code.length + 1);
+                                data.counters[code] = nam;
+                            }
                         }
+                    }
+                });
+            }
+            if ("localDBs" in config) {
+                config.localDBs.forEach((file) => loadDB(path + file).then(dat => {
+                    for (const cardData of dat) {
+                        const card = new Card(cardData, [file]);
+                        if (card.code in data.cards) {
+                            const dbs = card.dbs;
+                            dbs.push(file);
+                            card.dbs = dbs;
+                        }
+                        data.cards[card.code] = card;
+                    }
+                }, (err) => {
+                    console.error("Could not load database " + file + "!");
+                    console.error(err);
+                }));
+            }
+            if ("remoteDBs" in config) {
+                config.remoteDBs.forEach((repo) => {
+                    downloadRepo(repo, name).then((files) => {
+                        files.forEach((file) => loadDB(path + file).then(dat => {
+                            for (const cardData of dat) {
+                                const card = new Card(cardData, [file]);
+                                if (card.code in data.cards) {
+                                    const dbs = card.dbs;
+                                    dbs.push(file);
+                                    card.dbs = dbs;
+                                }
+                                data.cards[card.code] = card;
+                            }
+                        }, err => {
+                            console.error("Could not load database " + file + "!");
+                            console.error(err);
+                        }));
+                        // delete unused databases
+                        fs.readdir("dbs/" + name, (err, res) => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                res.filter((f) => !files.includes(f)).forEach((f) => {
+                                    fs.unlinkSync("dbs/" + name + "/" + f);
+                                });
+                            }
+                        });
                     });
                 });
-            });
-        }
+            }
+        });
+    }
+    public cards: { [n: number]: Card };
+    public setcodes: { [s: string]: string };
+    public counters: { [s: string]: string };
+    constructor(name, data) {
+        "";
     }
 }
