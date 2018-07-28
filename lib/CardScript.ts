bd12b6fe8a82d09ec69ea0e4acef290de4022037
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-28 15:03:00 +0930

    Update CardScript for async/await

--- lib/CardScript.ts
+++ lib/CardScript.ts
@@ -14,55 +14,44 @@ export class CardScript {
         };
     }
 
-    private update(): Promise<null> {
-        return new Promise((resolve, reject) => {
-            if (this.data) {
-                return resolve();
+    private async update(): Promise<void> {
+        if (this.data) {
+            return;
+        }
+        const content = await GitHub.repos.getContent(this.source);
+        for (const key in content.data) {
+            if (content.data.hasOwnProperty(key)) {
+                const file = content.data[key];
+                if (file.name === "c" + this.code + ".lua") {
+                    this.data = file;
+                    return;
+                }
+                throw new Error("Could not find script for " + this.code + "!");
             }
-            GitHub.repos
-                .getContent(this.source)
-                .then(content => {
-                    for (const key in content.data) {
-                        if (content.data.hasOwnProperty(key)) {
-                            const file = content.data[key];
-                            if (file.name === "c" + this.code + ".lua") {
-                                this.data = file;
-                                return resolve();
-                            }
-                        }
-                    }
-                    reject("Could not find script for " + this.code + "!");
-                })
-                .catch(e => reject(e));
-        });
+        }
     }
 
     get url(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            this.update()
-                .then(() => resolve(this.data.data.html_url))
-                .catch(e => reject(e));
+        return new Promise(async resolve => {
+            await this.update();
+            resolve(this.data.data.html_url);
         });
     }
 
     get content(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            this.update()
-                .then(() => resolve(this.data.data.content))
-                .catch(e => reject(e));
+        return new Promise(async resolve => {
+            await this.update();
+            resolve(this.data.data.content);
         });
     }
 
     get contentLines(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            this.update()
-                .then(() => {
-                    const lines: string[] = this.data.data.content.split(/\R/);
-                    const len = lines.length.toString().length;
-                    const script = lines.map((l, i) => (i + 1).toString().padStart(len, " ") + "|" + l).join("\n");
-                    resolve(script);
-                })
-                .catch(e => reject(e));
+        return new Promise(async (resolve, reject) => {
+            await this.update();
+            const lines: string[] = this.data.data.content.split(/\R/);
+            const len = lines.length.toString().length;
+            const script = lines.map((l, i) => (i + 1).toString().padStart(len, " ") + "|" + l).join("\n");
+            resolve(script);
         });
     }
 }
