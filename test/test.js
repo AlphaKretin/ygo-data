0311c4f7bffdfc514d6ea916f14865a69f26b7eb
AlphaKretin <deewhydeeecks@gmail.com>
2018-12-10 16:25:38 +1030

    Rewrite trivial tests for new structure (7)

--- test/test.js
+++ test/test.js
@@ -35,56 +35,56 @@ describe("Testing card list", function() {
 });
 describe("Testing OT", function() {
     it("Should be TCG", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.otNames).to.deep.equal(["TCG"]);
+        const card = await index.getCard(43694650);
+        expect(card.data.names.ot).to.deep.equal(["TCG"]);
     });
 });
 describe("Testing race", function() {
     it("Should be Beast", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.raceNames).to.deep.equal(["Beast"]);
+        const card = await index.getCard(43694650);
+        expect(card.data.names.race).to.deep.equal(["Beast"]);
     });
 });
 describe("Testing attribute", function() {
     it("Should be DARK", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.attributeNames).to.deep.equal(["Dark"]);
+        const card = await index.getCard(43694650);
+        expect(card.data.names.attribute).to.deep.equal(["Dark"]);
     });
 });
 describe("Testing ATK", function() {
     it("Should be 500", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.atk).to.equal(500);
+        const card = await index.getCard(43694650);
+        expect(card.data.atk).to.equal(500);
     });
 });
 describe("Testing DEF", function() {
     it("Should be 2000", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.def).to.equal(2000);
+        const card = await index.getCard(43694650);
+        expect(card.data.def).to.equal(2000);
     });
 });
 describe("Testing Setcodes", function() {
     it("Should be [Danger!]", async function() {
-        const card = await index.getCard(43694650, "en");
+        const card = await index.getCard(43694650);
         expect(card.setNames).to.deep.equal(["Danger!"]);
     });
 });
 describe("Testing types", function() {
     it("Should be Monster/Effect", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.typeNames).to.have.members(["Monster", "Effect"]);
+        const card = await index.getCard(43694650);
+        expect(card.data.names.type).to.have.members(["Monster", "Effect"]);
     });
 });
 describe("Testing type string", function() {
     it("Should be Beast/Effect", async function() {
-        const card = await index.getCard(43694650, "en");
+        const card = await index.getCard(43694650);
         expect(card.typeString).to.equal("Beast/Effect");
     });
 });
 describe("Testing categories", function() {
     it("Should be an array", async function() {
-        const card = await index.getCard(43694650, "en");
-        expect(card.categoryNames).to.be.a("array");
+        const card = await index.getCard(43694650);
+        expect(card.data.name.category).to.be.a("array");
     });
 });
 describe("Testing banlist", function() {
@@ -108,7 +108,7 @@ describe("Testing translation", function() {
 });
 describe("Testing image download", function() {
     it("Should be a buffer", async function() {
-        const card = await index.getCard(47346782, "en");
+        const card = await index.getCard(47346782);
         const image = await card.image;
         expect(image).to.be.instanceof(Buffer);
     });
